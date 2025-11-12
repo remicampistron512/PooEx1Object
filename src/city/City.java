47c7e4cdbb17d83ef44d8d4bd04c328dd339229b
Rémi Campistron <remi.campistron@gmail.com>
2025-11-12 11:19:05 +0100

    Exercice 1.2

--- src/city/City.java
+++ src/city/City.java
@@ -11,4 +11,41 @@ public class City {
         this.population = population;
     }
 
+    public City(String name, int population){
+        this.name = name;
+        this.population = population;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setPopulation(int population) {
+        if(population < 0)
+            throw new RuntimeException("La population ne peut pas être négative !");
+        this.population = population;
+    }
+
+    public void offsetPopulation(int offset){
+        if (offset < 0 && this.population + offset < 0){
+            throw new RuntimeException("La population ne peut pas être négative !");
+        } else {
+            this.population += offset;
+        }
+    }
 }
--- src/city/TestCity.java
+++ src/city/TestCity.java
@@ -2,13 +2,23 @@ package city;
 
 public class TestCity {
     public static void main(String[] args){
+        // Exercice 1.1
         City saintGeours = new City("Saint-geours de Maremne","France",2946);
         City toulouse = new City("Toulouse","France",450000);
         City merignac = new City("Merignac","France",77136);
 
-        System.out.println(saintGeours.name + ". population = " + saintGeours.population + ". Pays : " + saintGeours.country);
-        System.out.println(toulouse.name + ". population = " + toulouse.population + ". Pays : " + toulouse.country);
-        toulouse.population+=20000;
-        System.out.println(toulouse.name + ". population = " + toulouse.population + ". Pays : " + toulouse.country);
+        //System.out.println(saintGeours.name + ". population = " + saintGeours.population + ". Pays : " + saintGeours.country);
+        //System.out.println(toulouse.name + ". population = " + toulouse.population + ". Pays : " + toulouse.country);
+        //toulouse.population+=20000;
+        //System.out.println(toulouse.name + ". population = " + toulouse.population + ". Pays : " + toulouse.country);
+
+        //System.out.println(saintGeours.getName() + ". population = "+ saintGeours.getPopulation() + ". Pays : "+ saintGeours.getCountry());
+        //System.out.println(toulouse.getName() + ". population = "+ toulouse.getPopulation() + ". Pays : "+ toulouse.getCountry());
+        //System.out.println(merignac.getName() + ". population = "+ merignac.getPopulation() + ". Pays : "+ merignac.getCountry());
+
+        // Exercice 1.2
+        saintGeours.offsetPopulation(-500);
+
+        System.out.println(saintGeours.getName() + ". population = "+ saintGeours.getPopulation() + ". Pays : "+ saintGeours.getCountry());
     }
 }
